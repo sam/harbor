17414b5ed886be59ad69d9165cf372b540807e63
James Childers <james.childers@gmail.com>
2012-09-04 12:48:24 -0500

    Add more form examples to default app skeleton

--- lib/harbor/commands/skeletons/default/forms/example_form.rb
+++ lib/harbor/commands/skeletons/default/forms/example_form.rb
@@ -1,6 +1,16 @@
 
 class ExampleForm < Harbor::Form
+  # Available field types: string, text, password, integer, decimal, regex, email, file, boolean, null_boolean, choice, typed_choice, multiple_choice, radio_choice, and checkbox_multiple_choice.
+  # Fields are required by default
   string :first_name
   integer :age, min_value: 18
+  email :email
+  boolean :admin
+  regex :tele, /^\d{3}-\d{3}-\d{4}$/
+  choice :state, [['', 'Select a state'],
+                  ['AL', 'Alabama'],
+                  ['AK', 'Arkansas'],
+                  ['AR', 'Arizona']]
+  text :memo, label: 'Memo for this user', required: false
 end
 
